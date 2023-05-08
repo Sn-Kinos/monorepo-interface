62c55e85c9a499abc33478e5fdd90424bb786f1e
Sn-Kinos <dony435@naver.com>
2023-05-08 17:51:52 +0900

    build, change pnpm to npm

--- apps/app/index.ts
+++ apps/app/index.ts
@@ -3,6 +3,8 @@ import { LibraryInterface, LibraryType } from 'library';
 export const AppObjectWithInterface: LibraryInterface = {
   foo: '',
   relative: 1,
+  paths: [],
+  absolute: false,
 };
 
 export const AppObjectWithType: LibraryType = {
